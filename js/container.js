a0e4cca543b2d1cc81f243e28ab900ea33042900
michihiro <muta.michihiro@gmail.com>
2017-08-10 22:17:25 +0900

    add root component

---
+++ js/container.js
@@ -0,0 +1,11 @@
+
+angular.module('sample')
+  .component('container', {
+    templateUrl: 'view/container.html',
+    controller: containerCtrl,
+  });
+
+containerCtrl.$inject = [];
+
+function containerCtrl() {
+}
